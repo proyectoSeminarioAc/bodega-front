eb46781db887da74cb308736b1a3c969aa2fb8e5
xx1196 <50302178+xx1196@users.noreply.github.com>
2021-11-14 13:31:12 -0500

    Hotfix/2021 11 14 add style app bar (#4)
    * Se agrega paquete de rutas de react
    
    * Se agregan rutas y componente de providers
    
    * Se agregan las plantillas de las pantallas
    
    * Se agregan app bar
    
    * fix conflicts

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import {
     BrowserRouter as Router,
     Route,
-    Link,
     Switch,
 } from "react-router-dom";
 
@@ -12,31 +11,13 @@ import Storage from "./pages/storage";
 import Detail from "./pages/detail";
 import Product from "./pages/product";
 import Move from "./pages/move";
+import PrimaryAppBar from "./components/appBar";
 
 function App() {
     return (
         <Router>
             <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/providers">Proveedores</Link>
-                        </li>
-                        <li>
-                            <Link to="/product">Producto</Link>
-                        </li>
-                        <li>
-                            <Link to="/move">Movimiento</Link>
-                        </li>
-                        <li>
-                            <Link to="/storage">Bodega</Link>
-                        </li>
-                        <li>
-                            <Link to="/detail">Detalle</Link>
-                        </li>
-                    </ul>
-                </nav>
-
+               <PrimaryAppBar/>
                 <Switch>
                     <Route path="/providers">
                         <Provider/>
---
+++ src/components/appBar.js
@@ -0,0 +1,179 @@
+import {useHistory} from "react-router-dom";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Badge from '@mui/material/Badge';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MailIcon from '@mui/icons-material/Mail';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import MoreIcon from '@mui/icons-material/MoreVert';
+import {useState} from "react";
+import ConstructionIcon from '@mui/icons-material/Construction';
+import SupervisedUserCircleIcon from '@mui/icons-material/SupervisedUserCircle';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import TransitEnterexitIcon from '@mui/icons-material/TransitEnterexit';
+import DetailsIcon from '@mui/icons-material/Details';
+
+function PrimaryAppBar() {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState(null);
+    const history = useHistory();
+    const isMenuOpen = Boolean(anchorEl);
+    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+    const handleProfileMenuOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMobileMenuClose = () => {
+        setMobileMoreAnchorEl(null);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+        handleMobileMenuClose();
+    };
+
+    const handleMobileMenuOpen = (event) => {
+        setMobileMoreAnchorEl(event.currentTarget);
+    };
+
+    const menuId = 'primary-search-account-menu';
+    const renderMenu = (
+        <Menu
+            anchorEl={anchorEl}
+            anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            id={menuId}
+            keepMounted
+            transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            open={isMenuOpen}
+            onClose={handleMenuClose}
+        >
+            <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+            <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+        </Menu>
+    );
+
+    const mobileMenuId = 'primary-search-account-menu-mobile';
+    const renderMobileMenu = (
+        <Menu
+            anchorEl={mobileMoreAnchorEl}
+            anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            id={mobileMenuId}
+            keepMounted
+            transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            open={isMobileMenuOpen}
+            onClose={handleMobileMenuClose}
+        >
+            <MenuItem>
+                <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+                    <Badge badgeContent={4} color="error">
+                        <MailIcon/>
+                    </Badge>
+                </IconButton>
+                <p>Messages</p>
+            </MenuItem>
+            <MenuItem>
+                <IconButton
+                    size="large"
+                    aria-label="show 17 new notifications"
+                    color="inherit"
+                >
+                    <Badge badgeContent={17} color="error">
+                        <NotificationsIcon/>
+                    </Badge>
+                </IconButton>
+                <p>Notifications</p>
+            </MenuItem>
+            <MenuItem onClick={handleProfileMenuOpen}>
+                <IconButton
+                    size="large"
+                    aria-label="account of current user"
+                    aria-controls="primary-search-account-menu"
+                    aria-haspopup="true"
+                    color="inherit"
+                >
+                    <AccountCircle/>
+                </IconButton>
+                <p>Profile</p>
+            </MenuItem>
+        </Menu>
+    );
+
+    return (
+        <Box sx={{flexGrow: 1}}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="open drawer"
+                        sx={{mr: 2}}
+                    >
+                        <MenuIcon/>
+                    </IconButton>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="div"
+                        sx={{display: {xs: 'none', sm: 'block'}}}
+                    >
+                        BODEGA
+                    </Typography>
+                    <Box sx={{flexGrow: 1}}/>
+                    <Box sx={{display: {xs: 'none', md: 'flex'}}}>
+                        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+                            <SupervisedUserCircleIcon onClick={() => history.push('/providers')}/>
+                        </IconButton>
+                        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+                            <AddShoppingCartIcon onClick={() => history.push('/product')}/>
+                        </IconButton>
+                        <IconButton size="large" aria-label="show 17 new notifications" color="inherit">
+                            <TransitEnterexitIcon onClick={() => history.push('/move')}/>
+                        </IconButton>
+                        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+                            <ConstructionIcon onClick={() => history.push('/storage')}/>
+                        </IconButton>
+                        <IconButton size="large" aria-label="show 17 new notifications" color="inherit">
+                            <DetailsIcon onClick={() => history.push('/detail')}/>
+                        </IconButton>
+                    </Box>
+                    <Box sx={{display: {xs: 'flex', md: 'none'}}}>
+                        <IconButton
+                            size="large"
+                            aria-label="show more"
+                            aria-controls={mobileMenuId}
+                            aria-haspopup="true"
+                            onClick={handleMobileMenuOpen}
+                            color="inherit"
+                        >
+                            <MoreIcon/>
+                        </IconButton>
+                    </Box>
+                </Toolbar>
+            </AppBar>
+            {renderMobileMenu}
+            {renderMenu}
+        </Box>
+    );
+}
+
+export default PrimaryAppBar;

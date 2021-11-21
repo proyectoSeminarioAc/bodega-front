9e9eb70e9e55a7d14da72d258d3e9dc47ea866c2
Andrés Felipe Cardozo Cañas <andres.cardozo96@gmail.com>
2021-11-21 16:59:40 -0500

    Se añade crud de producto

--- src/pages/product/index.js
+++ src/pages/product/index.js
@@ -8,6 +8,10 @@ import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import {useHistory} from "react-router-dom";
+import {connect, useDispatch} from "react-redux";
+import {useEffect} from "react";
+import {deleteProductAction, getProductsAction} from "../../redux/productDuck";
+import SkeletonListComponent from "../../components/SkeletonComponent";
 
 function createData(name, calories, fat, carbs, protein) {
     return {name, calories, fat, carbs, protein};
@@ -21,8 +25,22 @@ const rows = [
     createData('Gingerbread', 356, 16.0, 49, 3.9),
 ];
 
-function Product() {
+function Product({products, deleteStatus, fetching}) {
     const history = useHistory();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getProductsAction())
+    }, [])
+
+    useEffect(() => {
+        if (deleteStatus)
+            dispatch(getProductsAction())
+    }, [deleteStatus])
+
+    const handleDelete = (id) => {
+        dispatch(deleteProductAction(id))
+    }
     return (
         <>
             <Button onClick={() => history.push('product/create')} variant="contained">Crear</Button>
@@ -31,31 +49,31 @@ function Product() {
                 <Table sx={{minWidth: 650}} aria-label="simple table">
                     <TableHead>
                         <TableRow>
-                            <TableCell>Dessert (100g serving)</TableCell>
-                            <TableCell align="right">Calories</TableCell>
-                            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                            <TableCell>id</TableCell>
+                            <TableCell align="right">Nombre</TableCell>
+                            <TableCell align="right">Descripcion</TableCell>
+                            <TableCell align="right">Referencia</TableCell>
                             <TableCell align="right">Opciones</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {rows.map((row) => (
+                        <SkeletonListComponent fetching={fetching}/>
+                        {products.map(({id, name, description, alternate_reference}) => (
                             <TableRow
-                                key={row.name}
+                                key={id}
                                 sx={{'&:last-child td, &:last-child th': {border: 0}}}
                             >
                                 <TableCell component="th" scope="row">
-                                    {row.name}
+                                    {id}
                                 </TableCell>
-                                <TableCell align="right">{row.calories}</TableCell>
-                                <TableCell align="right">{row.fat}</TableCell>
-                                <TableCell align="right">{row.carbs}</TableCell>
-                                <TableCell align="right">{row.protein}</TableCell>
+                                <TableCell align="right">{name}</TableCell>
+                                <TableCell align="right">{description}</TableCell>
+                                <TableCell align="right">{alternate_reference}</TableCell>
                                 <TableCell align="right">
                                     <ButtonGroup variant="contained" aria-label="outlined primary button group">
-                                        <Button onClick={() => history.push('/product/edit/1')}>Editar</Button>
-                                        <Button>Eliminar</Button>
+                                        <Button
+                                            onClick={() => history.push('/product/edit/' + id)}>Editar</Button>
+                                        <Button onClick={() => handleDelete(id)}>Eliminar</Button>
                                     </ButtonGroup>
                                 </TableCell>
                             </TableRow>
@@ -68,4 +86,13 @@ function Product() {
     );
 }
 
-export default Product;
+
+function mapState(state) {
+    return {
+        products: state.product.products,
+        deleteStatus: state.product.deleteStatus,
+        fetching: state.product.fetching,
+    }
+}
+
+export default connect(mapState)(Product);
---
+++ src/redux/productDuck.js
@@ -0,0 +1,173 @@
+import {exportHttpClient} from "../utils";
+
+const initialData = {
+    fetching: false,
+    product: null,
+    deleteStatus: false,
+    updateStatus: false,
+    products: [],
+    error: null
+}
+
+const httpClient = exportHttpClient();
+
+const FETCHING_PRODUCT = "FETCHING_PRODUCT";
+const FETCHING_PRODUCT_SUCCESS = "FETCHING_PRODUCT_SUCCESS";
+const FETCHING_PRODUCT_ERROR = "FETCHING_PRODUCT_ERROR";
+
+const FETCHING_PRODUCTS = "FETCHING_PRODUCTS";
+const FETCHING_PRODUCTS_SUCCESS = "FETCHING_PRODUCTS_SUCCESS";
+const FETCHING_PRODUCTS_ERROR = "FETCHING_PRODUCTS_ERROR";
+
+const CREATING_PRODUCT = "CREATING_PRODUCT";
+const CREATING_PRODUCT_SUCCESS = "CREATING_PRODUCT_SUCCESS";
+const CREATING_PRODUCT_ERROR = "CREATING_PRODUCT_ERROR";
+
+const UPDATING_PRODUCT = "UPDATING_PRODUCT";
+const UPDATING_PRODUCT_SUCCESS = "UPDATING_PRODUCT_SUCCESS";
+const UPDATING_PRODUCT_ERROR = "UPDATING_PRODUCT_ERROR";
+
+const DELETING_PRODUCT = "DELETING_PRODUCT";
+const DELETING_PRODUCT_SUCCESS = "DELETING_PRODUCT_SUCCESS";
+const DELETING_PRODUCT_ERROR = "DELETING_PRODUCT_ERROR";
+
+
+export default function productReducer(state = initialData, action) {
+    switch (action.type) {
+        case FETCHING_PRODUCT:
+        case FETCHING_PRODUCTS:
+        case CREATING_PRODUCT:
+        case UPDATING_PRODUCT:
+        case DELETING_PRODUCT:
+            return {
+                fetching: true,
+                product: null,
+                deleteStatus: false,
+                updateStatus: false,
+                products: [],
+                error: null
+            }
+
+        case FETCHING_PRODUCT_SUCCESS:
+        case CREATING_PRODUCT_SUCCESS:
+            return {...state, fetching: false, product: action.payload}
+
+        case UPDATING_PRODUCT_SUCCESS:
+            return {...state, fetching: false, product: action.payload, updateStatus: true}
+
+        case DELETING_PRODUCT_SUCCESS:
+            return {...state, fetching: false, deleteStatus: action.payload}
+
+        case FETCHING_PRODUCTS_SUCCESS:
+            return {...state, fetching: false, products: action.payload}
+
+        case FETCHING_PRODUCT_ERROR:
+        case FETCHING_PRODUCTS_ERROR:
+        case CREATING_PRODUCT_ERROR:
+        case UPDATING_PRODUCT_ERROR:
+        case DELETING_PRODUCT_ERROR:
+            return {...state, fetching: false, error: action.payload}
+        default:
+            return state;
+    }
+}
+
+export const getProductsAction = () => (dispatch, getState) => {
+    dispatch({
+        type: FETCHING_PRODUCTS
+    });
+
+    httpClient.get('/product/')
+        .then(({data}) => {
+            dispatch({
+                type: FETCHING_PRODUCTS_SUCCESS,
+                payload: data
+            })
+        }).catch(err => {
+        console.log(err)
+        dispatch({
+            type: FETCHING_PRODUCTS_ERROR,
+            payload: err
+        })
+    })
+}
+
+export const createProductAction = (dataForm) => (dispatch, getState) => {
+    dispatch({
+        type: CREATING_PRODUCT
+    });
+
+    httpClient.post('/product/', dataForm)
+        .then(({data}) => {
+            dispatch({
+                type: CREATING_PRODUCT_SUCCESS,
+                payload: data
+            })
+        }).catch(err => {
+        console.log(err)
+        dispatch({
+            type: CREATING_PRODUCT_ERROR,
+            payload: err
+        })
+    })
+}
+
+export const fetchProductAction = (id) => (dispatch, getState) => {
+    dispatch({
+        type: FETCHING_PRODUCT
+    });
+
+    httpClient.get('/product/' + id)
+        .then(({data}) => {
+            dispatch({
+                type: FETCHING_PRODUCT_SUCCESS,
+                payload: data
+            })
+        }).catch(err => {
+        console.log(err)
+        dispatch({
+            type: FETCHING_PRODUCT_ERROR,
+            payload: err
+        })
+    })
+}
+
+export const updateProductAction = (dataForm, id) => (dispatch, getState) => {
+    dispatch({
+        type: UPDATING_PRODUCT
+    });
+
+    httpClient.put('/product/' + id, dataForm)
+        .then(({data}) => {
+            dispatch({
+                type: UPDATING_PRODUCT_SUCCESS,
+                payload: data
+            })
+        }).catch(err => {
+
+        dispatch({
+            type: UPDATING_PRODUCT_ERROR,
+            payload: err
+        })
+    })
+}
+
+export const deleteProductAction = (id) => (dispatch, getState) => {
+    dispatch({
+        type: DELETING_PRODUCT
+    });
+
+    httpClient.delete('/product/' + id)
+        .then(({data}) => {
+            dispatch({
+                type: DELETING_PRODUCT_SUCCESS,
+                payload: data
+            })
+        }).catch(err => {
+
+        dispatch({
+            type: DELETING_PRODUCT_ERROR,
+            payload: err
+        })
+    })
+}

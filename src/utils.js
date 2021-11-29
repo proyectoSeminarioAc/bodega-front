e49ddbcfe83b67bdda4faf97e1065f5fe7a79f3b
Andrés Felipe Cardozo Cañas <andres.cardozo96@gmail.com>
2021-11-29 16:27:50 -0500

    Se agrega validacion d eurl

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export function exportHttpClient() {
     return axios.create({
-        baseURL: process.env.REACT_APP_API_URL,
+        baseURL: process.env.REACT_APP_API_URL||'http://ec2-3-19-72-67.us-east-2.compute.amazonaws.com:8080/api/project_store',
         timeout: 10000,
     });
 }
